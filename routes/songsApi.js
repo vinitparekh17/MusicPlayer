58c762affa64b31f99f59b0799b379ecac59675c
vinitparekh <vinitparekh1742@gmail.com>
2025-11-15 22:37:40 +0530

    keeping it as a backend only project

--- routes/songsApi.js
+++ routes/songsApi.js
@@ -4,27 +4,26 @@ const path = require('path');
 const router = express.Router();
 
 // Helper functions to read and write JSON data
-function readData(category) {
-  const DB_FILE = path.join(__dirname, '..', 'data', `${category}.json`);
+function readData() {
+  const DB_FILE = path.join(__dirname, '..', 'data', `songs.json`);
   return JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
 }
 
-function writeData(category, data) {
-  const DB_FILE = path.join(__dirname, '..', 'data', `${category}.json`);
+function writeData(data) {
+  const DB_FILE = path.join(__dirname, '..', 'data', `songs.json`);
   fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2), 'utf8');
 }
 
-// Get all songs by category (romance, hollywood, naughty)
-router.get('/songs/:category', (req, res) => {
-  const { category } = req.params;
-  const data = readData(category);
+// Get all songs
+router.get('/songs/', (req, res) => {
+  const data = readData();
   res.json(data);
 });
 
-// Get a single song by ID and category
-router.get('/songs/:category/:id', (req, res) => {
-  const { category, id } = req.params;
-  const data = readData(category);
+// Get a single song by ID
+router.get('/songs/:id', (req, res) => {
+  const { id } = req.params;
+  const data = readData();
   const song = data.find(s => s.id === parseInt(id));
   if (song) {
     res.json(song);
@@ -34,9 +33,8 @@ router.get('/songs/:category/:id', (req, res) => {
 });
 
 // POST a new song
-router.post('/songs/:category', (req, res) => {
-  const { category } = req.params;
-  const data = readData(category);
+router.post('/songs/', (req, res) => {
+  const data = readData();
   const newSong = {
     id: data.length ? data[data.length - 1].id + 1 : 1,
     songName: req.body.songName,
@@ -49,13 +47,13 @@ router.post('/songs/:category', (req, res) => {
 });
 
 // PUT to update a song by ID
-router.put('/songs/:category/:id', (req, res) => {
-  const { category, id } = req.params;
-  const data = readData(category);
+router.put('/songs//:id', (req, res) => {
+  const { id } = req.params;
+  const data = readData();
   const index = data.findIndex(s => s.id === parseInt(id));
   if (index !== -1) {
     data[index] = { ...data[index], ...req.body };
-    writeData(category, data);
+    writeData(data);
     res.json(data[index]);
   } else {
     res.status(404).json({ message: 'Song not found' });
@@ -63,13 +61,13 @@ router.put('/songs/:category/:id', (req, res) => {
 });
 
 // DELETE a song by ID
-router.delete('/songs/:category/:id', (req, res) => {
-  const { category, id } = req.params;
-  let data = readData(category);
+router.delete('/songs//:id', (req, res) => {
+  const { id } = req.params;
+  let data = readData();
   const index = data.findIndex(s => s.id === parseInt(id));
   if (index !== -1) {
     const deletedSong = data.splice(index, 1);
-    writeData(category, data);
+    writeData(data);
     res.json(deletedSong);
   } else {
     res.status(404).json({ message: 'Song not found' });
